7efaab977ae2175063f654e8e0ed4c05ecf90f65
SadiaMou007 <sadia.bu.cse@gmail.com>
2022-05-05 00:44:33 -0700

    add google signIn

--- src/Components/Authentication/Login/Login.js
+++ src/Components/Authentication/Login/Login.js
@@ -1,7 +1,31 @@
 import React from "react";
+import auth from "../../../Firebase/firebase.init";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import Loading from "../../Loading/Loading";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  let errorText;
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  if (user) {
+    navigate(from, { replace: true });
+  }
+  if (loading) {
+    return <Loading></Loading>;
+  }
+  if (error) {
+    errorText = <p className="text-danger text-center">{error.message}</p>;
+  }
+  return (
+    <div>
+      <button onClick={() => signInWithGoogle()}>Sign In with Google</button>
+      <div> {errorText}</div>
+    </div>
+  );
 };
 
 export default Login;
